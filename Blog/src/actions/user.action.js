605f85206aa77cff9a8683515af22e05f6d7b65e
kawlenou <awlenoualain@gmail.com>
2024-01-08 10:29:39 +0000

    blg avec du react js et redux

---
+++ Blog/src/actions/user.action.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const GET_USER = "GET_USER";
+export const ADD_USER_LIKE = "ADD_USER_LIKE";
+
+export const getUser = ()=>{
+    return(dispacth) =>{
+        return axios.get("http://localhost:3000/user").then((res) =>{
+            dispacth({
+                type : GET_USER,
+                payload : res.data[0]
+            })
+        })
+    }
+}
+export const addUserLike = (data)=>{
+    return(dispatch) =>{
+        return axios.put(`http://localhost:3000/user/${data.id}`, data).then((res) =>{
+            dispatch({
+                type : ADD_USER_LIKE,
+                payload : data
+            })
+        })
+    }
+}
